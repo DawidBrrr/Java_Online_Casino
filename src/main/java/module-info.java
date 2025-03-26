05a016201827a5b2266cacb3319a955718e5a88b
Dawid Brach <dbrach6@gmail.com>
2025-03-26 16:45:29 +0100

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.casino.java_online_casino {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.casino.java_online_casino to javafx.fxml;
+    exports com.casino.java_online_casino;
+}
